e4370a165271b4e032cd00b1c4a677ddd6240bc0
Martin Dahlgren <mdahlgrengadd@users.noreply.github.com>
2025-05-21 14:21:16 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import StartMenuItem from './StartMenuItem';
-import './Taskbar.css';
+import "./Taskbar.css";
+
+import React from "react";
 import {
-  FcHome, FcDocument, FcPicture, FcMusic, FcMultipleDevices,
-  FcGlobe, FcSettings, FcPrint, FcServiceMark, FcSearch,
-  FcCommandLine, FcInternal, FcInvite, FcViewDetails,
-  FcFolder, FcOpenedFolder
-} from 'react-icons/fc';
+  FcCommandLine,
+  FcDocument,
+  FcFolder,
+  FcGlobe,
+  FcHome,
+  FcInternal,
+  FcInvite,
+  FcMultipleDevices,
+  FcMusic,
+  FcOpenedFolder,
+  FcPicture,
+  FcPrint,
+  FcSearch,
+  FcServiceMark,
+  FcSettings,
+  FcViewDetails,
+} from "react-icons/fc";
+
+import StartMenuItem from "./StartMenuItem";
 
 interface StartMenuProps {
   isOpen: boolean;
@@ -14,147 +28,152 @@ interface StartMenuProps {
 
 const StartMenu = ({ isOpen }: StartMenuProps) => {
   if (!isOpen) return null;
-  
+
   return (
     <div className="taskbar-scope">
       <div className="absolute bottom-full left-0 w-[380px] h-[550px] bg-winxp-light-blue rounded-t-md shadow-lg z-[11000] overflow-hidden border border-border">
         {/* Header */}
         <div className="flex items-center h-[65px] px-2 py-2 bg-gradient-to-b from-winxp-medium-blue to-winxp-light-blue shadow-inner">
           <div className="bg-white rounded p-0.5 h-[52px] shadow-md">
-            <img 
-              src="/images/winxp-profile.jpg" 
-              alt="User" 
+            <img
+              src="/images/winxp-profile.png"
+              alt="User"
               className="h-full rounded"
             />
           </div>
-          <h1 className="text-white text-lg ml-2 drop-shadow-sm">John Smith</h1>
+          <h1 className="text-white text-lg ml-2 drop-shadow-sm">
+            B.J. Blaskowicz
+          </h1>
         </div>
-        
+
         {/* Body */}
         <div className="flex h-[calc(100%-65px-43px)] bg-white border border-winxp-light-blue mx-px relative">
           {/* Left Side - Favorites */}
           <div className="flex flex-col w-1/2 h-full p-2">
             {/* Internet */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcGlobe className="w-8 h-8" />}
               intent="Internet"
               program="Internet Explorer"
               type="intent"
             />
-            
+
             {/* Email */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcInvite className="w-8 h-8" />}
               intent="E-mail"
               program="Outlook Express"
               type="intent"
             />
-            
+
             <div className="my-1 h-px bg-gradient-to-r from-white via-gray-300 to-white" />
-            
+
             {/* Notepad */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcViewDetails className="w-8 h-8" />}
               label="Notepad"
               type="favorite"
             />
-            
+
             <div className="mt-auto">
               <div className="h-px bg-gradient-to-r from-white via-gray-300 to-white mb-1" />
               <div className="flex items-center justify-between font-bold text-winxp-menu-text-dark text-xs p-1 hover:bg-winxp-medium-blue hover:text-white cursor-pointer">
                 <span>All Programs</span>
-                <div className="w-4 h-4 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: "url('/images/arrow-right.ico')" }} />
+                <div
+                  className="w-4 h-4 bg-contain bg-no-repeat bg-center"
+                  style={{ backgroundImage: "url('/images/arrow-right.ico')" }}
+                />
               </div>
             </div>
           </div>
-          
+
           {/* Right Side - Shortcuts */}
           <div className="flex flex-col w-1/2 h-full p-2 bg-winxp-menu-blue border-l border-winxp-menu-border">
             {/* My Documents */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcDocument className="w-8 h-8" />}
               label="My Documents"
               type="my"
             />
-            
+
             {/* My Recent Documents */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcOpenedFolder className="w-8 h-8" />}
               label="My Recent Documents"
               type="my"
               hasSubmenu={true}
             />
-            
+
             {/* My Pictures */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcPicture className="w-8 h-8" />}
               label="My Pictures"
               type="my"
             />
-            
+
             {/* My Music */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcMusic className="w-8 h-8" />}
               label="My Music"
               type="my"
             />
-            
+
             {/* My Computer */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcMultipleDevices className="w-8 h-8" />}
               label="My Computer"
               type="my"
             />
-            
+
             {/* Network Places */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcGlobe className="w-8 h-8" />}
               label="My Network Places"
               type="my"
             />
-            
+
             <div className="my-1 h-px bg-gradient-to-r from-transparent via-[#aebad6] to-transparent" />
-            
+
             {/* Control Panel */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcSettings className="w-6 h-6" />}
               label="Control Panel"
               type="shortcut"
             />
-            
+
             {/* Printers */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcPrint className="w-6 h-6" />}
               label="Printers and Faxes"
               type="shortcut"
             />
-            
+
             <div className="my-1 h-px bg-gradient-to-r from-transparent via-[#aebad6] to-transparent" />
-            
+
             {/* Help */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcServiceMark className="w-6 h-6" />}
               label="Help and Support"
               type="shortcut"
             />
-            
+
             {/* Search */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcSearch className="w-6 h-6" />}
               label="Search"
               type="shortcut"
               hasSubmenu={true}
             />
-            
+
             {/* Run */}
-            <StartMenuItem 
+            <StartMenuItem
               customIcon={<FcCommandLine className="w-6 h-6" />}
               label="Run..."
               type="shortcut"
             />
           </div>
         </div>
-        
+
         {/* Footer */}
         <div className="h-[43px] bg-gradient-to-t from-winxp-medium-blue to-winxp-light-blue shadow-inner" />
       </div>
